6b94654dddccfde4c9f820e3827b05e51958e4d5
szymon-trybala <szymon.trybala@protonmail.com>
2020-03-25 20:24:13 +0100

    backup_input.rs completed, added docs and tests

--- src/lib/backups/helpers/hashing.rs
+++ src/lib/backups/helpers/hashing.rs
@@ -2,12 +2,20 @@ use std::fs::File;
 use std::io::{BufReader, Read};
 use std::path::Path;
 use meowhash::MeowHasher;
+use ring::digest::{Context, SHA256};
 use digest::Digest;
 
 /// Function generate hash of a file and return it as a string. Uses meow_hash, extremally fast non-cryptographic hasing method. This method of hashing is prefered in Flash Backup.
+///
 /// Returns error if file to which the path leads doesn't exist or is empty, exists but can't be opened, or if an error occurs during hashing.
-// TODO - add test
-pub fn generate_hash_meow_hash(path: &String) -> Result<String, String> {
+/// # Example (only for Linux, test may not pass if your bash file is different):
+/// ```
+/// use flash_backup::backups::helpers::hashing::generate_hash_meow_hash;
+/// let path = "/usr/bin/bash";
+/// let hash = generate_hash_meow_hash(path).unwrap();
+/// assert_eq!(hash, "1f0b7365561cc1809ad6016549e336234cd13758ef49fe5a474157c469f5a70533b1bc0c119e9bb0c552bcc0b80cd90c209c3b51af011fd4aa0ab474a1fb944b32f0dc02dd478794f52ad4754150669f4215152b3c1ae192b7db0b1899fc33c299d4f3b3c33a83f010d1d093297a7a50bad84806d81c87752298483f60de977b");
+/// ```
+pub fn generate_hash_meow_hash(path: &str) -> Result<String, String> {
     let as_path = Path::new(path);
     if !(as_path.exists() && as_path.is_file()) {
         let message = format!("Path {} doesn't exist or isn't a file", path);
@@ -44,45 +52,52 @@ pub fn generate_hash_meow_hash(path: &String) -> Result<String, String> {
     }
 }
 
-// /// Function generate hash of a file and return it as a string. Uses SHA-256, standard and widely used non-cryptographic hasing method.
-// /// Returns error if file to which the path leads doesn't exist or is empty, exists but can't be opened, or if an error occurs during hashing.
-// /// // TODO - add test
-// pub fn generate_hash_sha256(path: &String) -> Result<String, &'static str> {
-//     let as_path = Path::new(path);
-//     if !(as_path.exists() && as_path.is_file()) {
-//         let message = format!("Path {} doesn't exist or isn't a file", path);
-//         return Err(&message);
-//     }
-//
-//     match File::open(path) {
-//         Ok(file) => {
-//             let mut reader = BufReader::new(file);
-//             let mut context = Context::new(&SHA256);
-//             let mut buffer = [0; 1024];
-//
-//             loop {
-//                 match reader.read(&mut buffer) {
-//                     Ok(count) => {
-//                         if count == 0 {
-//                             break;
-//                         }
-//                         context.update(&buffer[..count])
-//                     }
-//                     Err(e) => {
-//                         let message = format!("Couldn't hash file {}: {}", path, e);
-//                         return Err(&message);
-//                     }
-//                 }
-//
-//             }
-//             let digest = context.finish();
-//
-//
-//             Ok(hex::encode(digest.as_ref()))
-//         }
-//         Err(e) => {
-//             let message = format!("Couldn't open file {} to generate its hash: {}", path, e);
-//             Err(&message)
-//         }
-//     }
-// }
+/// Function generate hash of a file and return it as a string. Uses SHA-256, standard and widely used non-cryptographic hasing method.
+///
+/// Returns error if file to which the path leads doesn't exist or is empty, exists but can't be opened, or if an error occurs during hashing.
+/// # Example (only for Linux, test may not pass if your bash file is different):
+/// ```
+/// use flash_backup::backups::helpers::hashing::generate_hash_sha256;
+/// let path = "/usr/bin/bash";
+/// let hash = generate_hash_sha256(path).unwrap();
+/// assert_eq!(hash, "fa834f012927e241e18ac016ddc3b352f848c0bd5fe98f21a1355c48b5518211");
+/// ```
+pub fn generate_hash_sha256(path: &str) -> Result<String, String> {
+    let as_path = Path::new(path);
+    if !(as_path.exists() && as_path.is_file()) {
+        let message = format!("Path {} doesn't exist or isn't a file", path);
+        return Err(message);
+    }
+
+    match File::open(path) {
+        Ok(file) => {
+            let mut reader = BufReader::new(file);
+            let mut context = Context::new(&SHA256);
+            let mut buffer = [0; 1024];
+
+            loop {
+                match reader.read(&mut buffer) {
+                    Ok(count) => {
+                        if count == 0 {
+                            break;
+                        }
+                        context.update(&buffer[..count])
+                    }
+                    Err(e) => {
+                        let message = format!("Couldn't hash file {}: {}", path, e);
+                        return Err(message);
+                    }
+                }
+
+            }
+            let digest = context.finish();
+
+
+            Ok(hex::encode(digest.as_ref()))
+        }
+        Err(e) => {
+            let message = format!("Couldn't open file {} to generate its hash: {}", path, e);
+            Err(message)
+        }
+    }
+}
--- src/lib/backups/traits/backup_input.rs
+++ src/lib/backups/traits/backup_input.rs
@@ -1,58 +1,118 @@
 use crate::backups::map::backup_dir::BackupDir;
-use std::path::{Path};
+use std::path::Path;
 use walkdir::WalkDir;
 use crate::backups::map::backup_entry::BackupEntry;
 use crate::backups::helpers::hashing::generate_hash_meow_hash;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::borrow::BorrowMut;
+use scoped_threadpool::Pool;
 
 #[cfg(test)]
-#[test]
-fn it_works() {
-    // let dir = vec![BackupDir { root_input: String::from("home/szymon/Downloads/meowhash-rs"), folders: 0, files: 0, root_output: String::new(), backup_entries: Vec::new() }];
-    let dir = check_input_folders(&vec![String::from("/home/szymon/Downloads/meowhash-rs")]);
-    let mut backup_dirs = Arc::new(Mutex::new(dir));
-    let dir = fill_backup_dirs_parallel(backup_dirs);
-    let dir = Arc::try_unwrap(dir);
-    let dir = dir.unwrap_or_default();
-    let dir = dir.into_inner().unwrap_or_default();
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_check_input_folders() {
+        let paths = vec![String::from("/usr/include/bash")];
+        let result = check_input_folders(&paths);
+        assert_eq!(result[0].root_input, "/usr/include/bash");
+    }
+
+    #[test]
+    fn test_fill_single_backup_dir() {
+        let mut dir = BackupDir { root_input: String::from("/usr/include/bash"), root_output: String::new(), files: 0, folders: 0, backup_entries: vec![] };
+        fill_single_backup_dir(&mut dir);
+        assert_eq!(dir.root_input, "/usr/include/bash");
+        assert_eq!(dir.files, 58);
+        assert_eq!(dir.folders, 2);
+    }
+
+    #[test]
+    fn test_fill_backup_dirs_parallel_and_arc_to_inner() {
+        let dirs = vec![BackupDir { root_input: String::from("/usr/include/bash"), root_output: String::new(), files: 0, folders: 0, backup_entries: vec![] }];
+        let backup_dirs = Arc::new(Mutex::new(dirs));
+        let backup_dirs = fill_backup_dirs_parallel(backup_dirs);
+        let backup_dirs = arc_to_inner(backup_dirs).unwrap();
+        assert_eq!(backup_dirs[0].root_input, "/usr/include/bash");
+        assert_eq!(backup_dirs[0].files, 58);
+        assert_eq!(backup_dirs[0].folders, 2);
+    }
+
+    #[test]
+    fn test_all() {
+        let paths = vec![String::from("/usr/bin/core_perl"), String::from("/usr/share/alsa"), String::from("/usr/share/gtk-doc"), String::from("/usr/share/help"), String::from("/usr/lib32/pulseaudio")];
+        let dirs = check_input_folders(&paths);
+        let dirs = Arc::new(Mutex::new(dirs));
+        let dirs = fill_backup_dirs_parallel(dirs);
+        let dirs = arc_to_inner(dirs).unwrap();
+        assert_eq!(dirs.len(), 5);
+        println!("EHS {}", dirs[1].files);
+    }
 }
 
+/// Provides function to create and fill folder maps with all needed input data
 pub trait BackupInput {
-    // TODO - doc, test
-    fn create_input_maps(paths: &Vec<String>) -> Result<Vec<BackupDir>, String> {
+    /// Creates file map as Vec<BackupDir> based on input paths, ensures that no BackupDir is empty. Creating maps and generating hashes is done parallelly.
+    ///
+    /// Function may panic in few cases: if all input paths are invalid, if maps are empty, or if fatal error occurs during multithreading operations and conversions. If error occurs while processing/hashing some file,
+    /// user gets message and file is skipped.
+    ///
+    /// Example (only for Linux, test may fail if your /usr/include/bash is different):
+    /// ```
+    /// use std::sync::{Arc, Mutex};
+    /// use flash_backup::backups::traits::backup_input::{check_input_folders, fill_backup_dirs_parallel, arc_to_inner};
+    /// let dir = check_input_folders(&vec![String::from("/usr/include/bash")]);
+    /// let mut backup_dirs = Arc::new(Mutex::new(dir));
+    /// let dir = fill_backup_dirs_parallel(backup_dirs);
+    /// let dir = arc_to_inner(dir).unwrap();
+    /// assert_eq!(dir[0].root_input, "/usr/include/bash");
+    /// assert_eq!(dir[0].files, 58);
+    /// assert_eq!(dir[0].folders, 2);
+    /// ```
+    fn create_input_maps(paths: &Vec<String>) -> Vec<BackupDir> {
         let backup_dirs = check_input_folders(paths);
         let mut backup_dirs = Arc::new(Mutex::new(backup_dirs));
         backup_dirs = fill_backup_dirs_parallel(backup_dirs);
-        // TODO - ugly, fix it
-        let backup_dirs = Arc::try_unwrap(backup_dirs).unwrap_or_default().into_inner().unwrap_or_default();
-        Ok(backup_dirs)
-        // TODO - check for errors, return something
+        match arc_to_inner(backup_dirs) {
+            Ok(dirs) => {
+                if !dirs.is_empty() {
+                    dirs
+                } else {
+                    panic!("Fatal error while trying to create input maps - all maps are empty");
+                }
+            }
+            Err(e) => {
+                let message = format!("Fatal error while trying to create input maps - {}", e);
+                panic!(message);
+            }
+        }
     }
 }
 
 /// Returns Vec<Backup_Dir> filled with Backup_Dir for every passed valid and non-empty path, only present field in Backup_Dir's is root_input.
+///
 /// Panics if there are no valid and non-empty input folders - there is no point of continuing the program if this happens
-/// # Examples:
-/// Replace "szymon" with your username. Test only for Linux.
-// TODO - znaleźć jakiś plik na każdym linuksie i zrobić test tylko dla niego.
+/// # Example (works only on Linux, test may fail if your system doesn't have /usr/include/bash):
 /// ```
-/// let paths = vec![String::from("/home/szymon/Downloads")];
+/// use flash_backup::backups::traits::backup_input::check_input_folders;
+///
+/// let paths = vec![String::from("/usr/include/bash")];
 /// let result = check_input_folders(&paths);
-/// assert_eq!(result[0].root_input, "home/szymon/Downloads");
-fn check_input_folders(folders: &Vec<String>) -> Vec<BackupDir> {
+/// assert_eq!(result[0].root_input, "/usr/include/bash");
+/// ```
+pub fn check_input_folders(folders: &Vec<String>) -> Vec<BackupDir> {
     let mut dirs = Vec::new();
 
     for folder in folders {
         match Path::new(folder).exists() {
             true => {
-                dirs.push(BackupDir {root_input: folder.clone(), root_output: String::new(), folders: 0, files: 0, backup_entries: Vec::new()})
+                dirs.push(BackupDir { root_input: folder.clone(), root_output: String::new(), folders: 0, files: 0, backup_entries: Vec::new() })
             }
             false => {
                 println!("Folder {} doesn't exist or is empty, skipping...", folder);
                 continue;
-            },
+            }
         }
     }
 
@@ -63,11 +123,27 @@ fn check_input_folders(folders: &Vec<String>) -> Vec<BackupDir> {
 }
 
 /// Fills data in every BackupDir concurrently - one thread per one folder with maximum amount of present threads equal to amount of threads in computer.
+///
 /// Function handles creating threads, and then for every one of them it executes fill_single_backup_dir - for further info check its documentation.
+///
 /// Requires owned vector wrapped in Arc<Mutex>, returns filled vector, also as owned value. Vector is always returned.
+///
 /// If vector of BackupDir's is empty, it prints message and returns unchanged vector.
-// TODO - add test
-fn fill_backup_dirs_parallel(dirs: Arc<Mutex<Vec<BackupDir>>>) -> Arc<Mutex<Vec<BackupDir>>>  {
+/// # Example (works only on Linux, test may fail if your /usr/include/bash is different):
+/// ```
+/// use flash_backup::backups::map::backup_dir::BackupDir;
+/// use std::sync::{Arc, Mutex};
+/// use flash_backup::backups::traits::backup_input::fill_backup_dirs_parallel;
+///
+/// let dirs = vec![BackupDir { root_input: String::from("/usr/include/bash"), root_output: String::new(), files: 0, folders: 0, backup_entries: vec![] }];
+/// let backup_dirs = Arc::new(Mutex::new(dirs));
+/// let backup_dirs = fill_backup_dirs_parallel(backup_dirs);
+/// let backup_dirs = Arc::try_unwrap(backup_dirs).unwrap_or_default().into_inner().unwrap_or_default();
+/// assert_eq!(backup_dirs[0].root_input, "/usr/include/bash");
+/// assert_eq!(backup_dirs[0].files, 58);
+/// assert_eq!(backup_dirs[0].folders, 2);
+/// ```
+pub fn fill_backup_dirs_parallel(dirs: Arc<Mutex<Vec<BackupDir>>>) -> Arc<Mutex<Vec<BackupDir>>> {
     // Checking input
     let dirs_ref = Arc::clone(&dirs);
     if dirs_ref.lock().unwrap().is_empty() {
@@ -76,32 +152,40 @@ fn fill_backup_dirs_parallel(dirs: Arc<Mutex<Vec<BackupDir>>>) -> Arc<Mutex<Vec<
     }
 
     // Creating threads and executing fill_single_backup_dir function on every of them
-    let mut handles = vec![];
+    println!("Creating maps");
     let len = dirs_ref.lock().unwrap().len();
-    // TODO - restrict threads amount (use thread count)
-    for i in 0..len {
-        let dirs_ref = Arc::clone(&dirs);
-        let handle = thread::spawn(move || {
-            let mut dirs_temp = dirs_ref.lock().unwrap();
-            fill_single_backup_dir(dirs_temp[i].borrow_mut());
-        });
-        handles.push(handle);
-    }
-    for handle in handles {
-        // TODO - co zrobić jak join nie zadziała :c
-        handle.join();
-    }
+    let max_threads = num_cpus::get();
+    let mut thread_pool = Pool::new(max_threads as u32);
+    thread_pool.scoped(|scoped| {
+        for i in 0..len {
+            let dirs_ref = Arc::clone(&dirs);
+            scoped.execute(move || {
+                let mut dirs_temp = dirs_ref.lock().unwrap();
+                fill_single_backup_dir(dirs_temp[i].borrow_mut());
+            });
+        }
+    });
     dirs
 }
 
-
-/// Fills data of one BackupDir. FUNCTION ASSUMS THAT root_input IS ALREADY FILLED, if it isn't, it doesn't modify directory (and informs user about it)
+/// Fills data of one BackupDir.
+///
+/// **FUNCTION ASSUMS THAT root_input IS ALREADY FILLED, if it isn't, it doesn't modify directory (and informs user about it)**
 /// Filled fields: folders, files, backup_entries (function creates Vec of BackupEntry, each one has input_path, is_file and hash filled).
 ///
 /// Function may panic while converting DirEntry.path() to str, but it's almost impossible.
 /// Function skip files for which hash couldn't be generated, user gets info about every skipped file.
-// TODO - add test
-fn fill_single_backup_dir(dir: &mut BackupDir) {
+/// # Example (works only on Linux, test may fail if your /usr/include/bash is different):
+/// ```
+/// use flash_backup::backups::map::backup_dir::BackupDir;
+/// use flash_backup::backups::traits::backup_input::fill_single_backup_dir;
+/// let mut dir = BackupDir {root_input: String::from("/usr/include/bash"), root_output: String::new(), files: 0, folders: 0, backup_entries: vec![]};
+/// fill_single_backup_dir(&mut dir);
+/// assert_eq!(dir.root_input, "/usr/include/bash");
+/// assert_eq!(dir.files, 58);
+/// assert_eq!(dir.folders, 2);
+/// ```
+pub fn fill_single_backup_dir(dir: &mut BackupDir) {
     // Checking input
     let as_path = Path::new(&dir.root_input);
     if dir.root_input.is_empty() || !as_path.exists() || as_path.is_file() {
@@ -110,13 +194,13 @@ fn fill_single_backup_dir(dir: &mut BackupDir) {
     }
 
     // Creating map
-    for entry in WalkDir::new(&dir.root_input).into_iter().skip(1).filter_map(|e| e.ok()) {
+    for entry in WalkDir::new(&dir.root_input).into_iter().skip(1).filter_map(|e| e.ok()) { // skip(1) because first value is always root input
         match entry.path().is_file() {
             true => {
                 let path = String::from(entry.path().to_str().expect("Unexpected error while creating maps"));
                 match generate_hash_meow_hash(&path) {
                     Ok(hash) => {
-                        dir.backup_entries.push(BackupEntry {input_path: path, output_path: String::new(), is_file: true, hash })
+                        dir.backup_entries.push(BackupEntry { input_path: path, output_path: String::new(), is_file: true, hash })
                     }
                     Err(e) => {
                         println!("{}, skipping...", e);
@@ -126,7 +210,7 @@ fn fill_single_backup_dir(dir: &mut BackupDir) {
             }
             false => {
                 let path = String::from(entry.path().to_str().expect("Unexpected error while creating maps"));
-                dir.backup_entries.push( BackupEntry {input_path: path, output_path: String::new(), is_file: false, hash: String::new()});
+                dir.backup_entries.push(BackupEntry { input_path: path, output_path: String::new(), is_file: false, hash: String::new() });
             }
         }
     }
@@ -138,5 +222,34 @@ fn fill_single_backup_dir(dir: &mut BackupDir) {
         println!("Couldn't find any entries in {}", &dir.root_input);
     } else {
         println!("Found {} files and {} folders in {}", dir.files, dir.folders, &dir.root_input)
+    }
+}
+
+/// Unwraps consumed Arc<Mutex<Vec<BackupDir>>> (first Arc to Mutex, then Mutex to Vec) and returns owned Vec<BackupDir> or an error if Vec couldn't be acquired.
+/// # Example (works only on Linux, test may fail if your /usr/include/bash is different):
+/// ```
+/// use flash_backup::backups::map::backup_dir::BackupDir;
+/// use std::sync::{Arc, Mutex};
+/// use flash_backup::backups::traits::backup_input::arc_to_inner;
+///
+/// let dirs = vec![BackupDir { root_input: String::from("/usr/include/bash"), root_output: String::new(), files: 0, folders: 0, backup_entries: vec![] }];
+/// let backup_dirs = Arc::new(Mutex::new(dirs));
+/// let backup_dirs = arc_to_inner(backup_dirs).unwrap();
+/// assert_eq!(backup_dirs[0].root_input, "/usr/include/bash");
+/// ```
+pub fn arc_to_inner(arc: Arc<Mutex<Vec<BackupDir>>>) -> Result<Vec<BackupDir>, String> {
+    match Arc::try_unwrap(arc) {
+        Ok(mutex) => {
+            match mutex.into_inner() {
+                Ok(dir) => {
+                    Ok(dir)
+                }
+                Err(e) => {
+                    let message = format!("Couldn't unwrap Mutex to inner: {}", e);
+                    Err(message)
+                }
+            }
+        }
+        Err(_) => Err(String::from("Couldn't unwrap Arc to Mutex"))
     }
 }
